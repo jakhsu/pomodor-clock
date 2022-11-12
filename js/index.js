5b5ea4fe0d11d55baf1cc63497ed6520d5834d62
jackhsu <contactjackhsu@gmail.com>
2022-11-12 17:45:58 +0000

    first commit

--- js/index.js
+++ js/index.js
@@ -31,55 +31,64 @@ var bgRed = "#f44336";
  * and change color depending on session.
  */
 function display(deadline) {
-	var time = formatTime(deadline);
-	
-	// calculate % of elapsed time
-	if (state == "start" || state == "pause")
-		var percentage = (((workTimeDef * 60) - remainingTime) / (workTimeDef * 60) * 100);
-	else if ((count % 4 == 0) && (state == "break" || state == "pauseBreak"))
-		var percentage = (((longBreakTimeDef * 60) - remainingTime) / (longBreakTimeDef * 60) * 100);
-	else if (state == "break" || state == "pauseBreak")
-		var percentage =(((breakTimeDef * 60) - remainingTime) / (breakTimeDef * 60) * 100);
-	else
-		percentage = 0;
-	
-	// fill timer background based on % of elapsed time and session.
-	$(".filler").css({background: "linear-gradient(to top, "+bgColor+ " " + percentage+"%,transparent "+percentage+"%,transparent 100%)"});
-	$(".pomodoro").css({border: "3px "+bgColor+" solid"});
-	
-	// update work time and break time (no change if no '+' or '-' clicked)
-	$(".work-time").text(workTimeDef);
-	$(".break-time").text(breakTimeDef);
-	$(".long-break-time").text(longBreakTimeDef);
-	
-	// Create string with HTML depending on state and time remaining
-    // and update page title according to session/timer state
-	var str;
-	switch(state) {
-		case "start":
-            $(".title").text(time + " - Work");
-			str = "<h1>Work</h1><br><h2>" + time + "</h2>";
-			break;
-		case "pause":
-            $(".title").text(time + " - Paused (Work)");
-			str = "<h1>Unpause</h1><br><h2>" + time + "</h2>";
-			break;
-		case "break":
-            $(".title").text(time + " - Break");
-			if (count % 4 == 0)
-				str = "<h1>Long Break</h1><br><h2>" + time + "</h2>";
-			else
-				str = "<h1>Break</h1><br><h2>" + time + "</h2>";
-			break;
-		case "pauseBreak":
-            $(".title").text(time + " - Paused (Break)");
-			str = "<h1>Unpause Break</h1><br><h2>" + time + "</h2>";
-			break;
-		default:
-			str = "<h1>Start</h1><br><h2>" + workTimeDef + ":00</h2>";
-				}
-	// display the string in HTML
-	$(".filler").html(str);
+  var time = formatTime(deadline);
+
+  // calculate % of elapsed time
+  if (state == "start" || state == "pause")
+    var percentage =
+      ((workTimeDef * 60 - remainingTime) / (workTimeDef * 60)) * 100;
+  else if (count % 4 == 0 && (state == "break" || state == "pauseBreak"))
+    var percentage =
+      ((longBreakTimeDef * 60 - remainingTime) / (longBreakTimeDef * 60)) * 100;
+  else if (state == "break" || state == "pauseBreak")
+    var percentage =
+      ((breakTimeDef * 60 - remainingTime) / (breakTimeDef * 60)) * 100;
+  else percentage = 0;
+
+  // fill timer background based on % of elapsed time and session.
+  $(".filler").css({
+    background:
+      "linear-gradient(to top, " +
+      bgColor +
+      " " +
+      percentage +
+      "%,transparent " +
+      percentage +
+      "%,transparent 100%)",
+  });
+  $(".pomodoro").css({ border: "3px " + bgColor + " solid" });
+
+  // update work time and break time (no change if no '+' or '-' clicked)
+  $(".work-time").text(workTimeDef);
+  $(".break-time").text(breakTimeDef);
+  $(".long-break-time").text(longBreakTimeDef);
+
+  // Create string with HTML depending on state and time remaining
+  // and update page title according to session/timer state
+  var str;
+  switch (state) {
+    case "start":
+      $(".title").text(time + " - Work");
+      str = "<h1>Work</h1><br><h2>" + time + "</h2>";
+      break;
+    case "pause":
+      $(".title").text(time + " - Paused (Work)");
+      str = "<h1>Unpause</h1><br><h2>" + time + "</h2>";
+      break;
+    case "break":
+      $(".title").text(time + " - Break");
+      if (count % 4 == 0) str = "<h1>Long Break</h1><br><h2>" + time + "</h2>";
+      else str = "<h1>Break</h1><br><h2>" + time + "</h2>";
+      break;
+    case "pauseBreak":
+      $(".title").text(time + " - Paused (Break)");
+      str = "<h1>Unpause Break</h1><br><h2>" + time + "</h2>";
+      break;
+    default:
+      str = "<h1>Start</h1><br><h2>" + workTimeDef + ":00</h2>";
+  }
+  // display the string in HTML
+  $(".filler").html(str);
 }
 
 /**
@@ -88,28 +97,27 @@ function display(deadline) {
  *and runs the appropriate function.
  */
 function toggle() {
-	
-	switch(state) {
-		case "start":
-			state = "pause";
-			pause();
-			break;
-		case "pause":
-			state = "start";
-			start();
-			break;
-		case "break":
-			state = "pauseBreak";
-			pauseBreak();
-			break;
-		case "pauseBreak":
-			state = "break";
-			startBreak();
-			break;
-		default:
-			remainingTime = workTimeDef * 60;
-			start();
-				}
+  switch (state) {
+    case "start":
+      state = "pause";
+      pause();
+      break;
+    case "pause":
+      state = "start";
+      start();
+      break;
+    case "break":
+      state = "pauseBreak";
+      pauseBreak();
+      break;
+    case "pauseBreak":
+      state = "break";
+      startBreak();
+      break;
+    default:
+      remainingTime = workTimeDef * 60;
+      start();
+  }
 }
 
 /**
@@ -117,9 +125,8 @@ function toggle() {
  * in a usable format (ms)
  */
 function getRemainingTime() {
-
-	var time = remainingTime * 1000;
-	return time;
+  var time = remainingTime * 1000;
+  return time;
 }
 
 /**
@@ -127,222 +134,214 @@ function getRemainingTime() {
  * and runs updateClock()
  */
 function initializeClock(deadline) {
-
-	/**
-	 * updateClock() is the function run in
-	 * every interval. It gets the remaining time,
-	 * runs display(), and checks the state to see
-	 * if it needs to stop the intervals, plays a sound if
-	 * the session is done, and handles long break every 4 
-	 * pomodoros.
-	 */
-  	function updateClock() {
-    	var time = getRemainingTime();
+  /**
+   * updateClock() is the function run in
+   * every interval. It gets the remaining time,
+   * runs display(), and checks the state to see
+   * if it needs to stop the intervals, plays a sound if
+   * the session is done, and handles long break every 4
+   * pomodoros.
+   */
+  function updateClock() {
+    var time = getRemainingTime();
 
     display(deadline);
-		
-	// if the timer is running, reduce remaining time by 1 second
-	if (state != "pause" && state != "pauseBreak")
-		remainingTime--;
-	else // if paused, stop the intervals
-		clearInterval(timeInterval);
-	  
-	// checks if the current session is over	
+
+    // if the timer is running, reduce remaining time by 1 second
+    if (state != "pause" && state != "pauseBreak") remainingTime--;
+    // if paused, stop the intervals
+    else clearInterval(timeInterval);
+
+    // checks if the current session is over
     if (time <= 0) {
-		// plays a bell sound
-		var audio = new Audio("./audio/bell.mp3");
-		audio.play();
-		// stops the intervals
-     	clearInterval(timeInterval);
-		
-		// checks which session we are at
-		if (state == "start") { // if we were on a work session,
-			//increment completed pomodoro counter
-			count++;
-			// long break if counter is multiple of 4
-			if (count % 4 == 0)
-				remainingTime = longBreakTimeDef * 60;
-			else // if not, normal break
-				remainingTime = breakTimeDef * 60;
-			
-			// change state to break and start the break timer
-			state = "break";
-			startBreak();
-		}
-		else { // if we were on a break
-			// update remaining time, change state and start work timer
-			remainingTime = workTimeDef * 60;
-			state = "start";
-			start();
-		}
+      // plays a bell sound
+      var audio = new Audio("./audio/cyberpunk_incomingcall.mp3");
+      audio.play();
+      // stops the intervals
+      clearInterval(timeInterval);
+
+      // checks which session we are at
+      if (state == "start") {
+        // if we were on a work session,
+        //increment completed pomodoro counter
+        count++;
+        // long break if counter is multiple of 4
+        if (count % 4 == 0) remainingTime = longBreakTimeDef * 60;
+        // if not, normal break
+        else remainingTime = breakTimeDef * 60;
+
+        // change state to break and start the break timer
+        state = "break";
+        startBreak();
+      } else {
+        // if we were on a break
+        // update remaining time, change state and start work timer
+        remainingTime = workTimeDef * 60;
+        state = "start";
+        start();
+      }
     }
   }
-	
-	// runs updateClock in intervals of 1 sec
-  	updateClock();
-  	timeInterval = setInterval(updateClock, 1000);
-}
 
+  // runs updateClock in intervals of 1 sec
+  updateClock();
+  timeInterval = setInterval(updateClock, 1000);
+}
 
 /**
  * formatTime() returns a string with the remaining
  * time in a nice printable format.
  */
 function formatTime(deadline) {
-	time = getRemainingTime(deadline);
-	var timeStr = "";
-	// calculate seconds, minutes and hours left.
-    var s = Math.floor((time / 1000) % 60);
-  	var m = Math.floor((time / 1000 / 60) % 60);
-  	var h = Math.floor((time / (1000 * 60 * 60)) % 24);
-	if (h > 0) 
-		timeStr += h + ":";
-	if (m >= 10)
-		timeStr += m + ":";
-	else
-		timeStr += "0" + m + ":";
-	if (s >= 10)
-		timeStr += s;
-	else
-		timeStr += "0" + s;
-    
-	return timeStr;
-  }
+  time = getRemainingTime(deadline);
+  var timeStr = "";
+  // calculate seconds, minutes and hours left.
+  var s = Math.floor((time / 1000) % 60);
+  var m = Math.floor((time / 1000 / 60) % 60);
+  var h = Math.floor((time / (1000 * 60 * 60)) % 24);
+  if (h > 0) timeStr += h + ":";
+  if (m >= 10) timeStr += m + ":";
+  else timeStr += "0" + m + ":";
+  if (s >= 10) timeStr += s;
+  else timeStr += "0" + s;
+
+  return timeStr;
+}
 
 /**
  * start() starts the work timer.
  */
 function start() {
-    // clear interval if already running
-    clearInterval(timeInterval);
-	// get an absolute date based on remaining time
-	deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
-	// change state and timer background color
-	state = "start";
-	bgColor = bgGreen;
-	// start the intervals for the work session
-	initializeClock(deadline);
+  // clear interval if already running
+  clearInterval(timeInterval);
+  // get an absolute date based on remaining time
+  deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
+  // change state and timer background color
+  state = "start";
+  bgColor = bgGreen;
+  // start the intervals for the work session
+  initializeClock(deadline);
 }
 
 /**
  * startBreak() starts the break timer.
  */
 function startBreak() {
-    // clear interval if already running
-    clearInterval(timeInterval);
-	// get an absolute date based on remaining time
-	deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
-	// change state and timer background color
-	state = "break";
-	bgColor = bgRed;
-	// start the intervals for the break session
-	initializeClock(deadline);
-	
+  // clear interval if already running
+  clearInterval(timeInterval);
+  // get an absolute date based on remaining time
+  deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
+  // change state and timer background color
+  state = "break";
+  bgColor = bgRed;
+  // start the intervals for the break session
+  initializeClock(deadline);
 }
 
 /**
  * pause() stops the work timer intervals and "freezes" time
  */
 function pause() {
-	clearInterval(timeInterval);
-	state = "pause";
-	
-	display(deadline);
+  clearInterval(timeInterval);
+  state = "pause";
+
+  display(deadline);
 }
 
 /**
  * pause() stops the break timer intervals and "freezes" time
  */
 function pauseBreak() {
-	clearInterval(timeInterval);
-	state = "pauseBreak";
-	
-	display(deadline);
+  clearInterval(timeInterval);
+  state = "pauseBreak";
+
+  display(deadline);
 }
 
-
 // this is ran when page is loaded.
-$(document).ready(function() {
-	
-	// when timer is clicked, change state
-	$(".filler").click(function(){
-		toggle();
-	});
-	
-	// add a minute to work session default & remaining time
-	$(".work-plus").click(function(){
-		if (state == "" || state == "pause" || state == "pauseBreak") {
-			workTimeDef++;
-			if (state == "pause")
-				remainingTime += 60;
-			deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
-			display(deadline);
-		}
-	});
-	
-	// subtract a minute from work session default & remaining time
-	$(".work-minus").click(function(){
-		if (remainingTime > 61 && workTimeDef > 1 && (state == "" || state == "pause" || state == "pauseBreak") ) {
-			workTimeDef--;
-			if (state == "pause")
-				remainingTime -= 60;
-			deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
-			display(deadline);
-		}
-	});
-	
-	// add a minute to break session default & remaining time
-	$(".break-plus").click(function(){
-		if (state == "" || state == "pause" || state == "pauseBreak") {
-			breakTimeDef++;
-			if (state == "pauseBreak")
-				remainingTime += 60;
-			deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
-			display(deadline);
-		}
-	});
-	
-	// subtract a minute from break session default & remaining time
-	$(".break-minus").click(function(){
-		if (remainingTime > 61 && breakTimeDef > 1 && (state == "" || state == "pause" || state == "pauseBreak") ) {
-			breakTimeDef--;
-			if (state == "pauseBreak")
-				remainingTime -= 60;
-			deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
-			display(deadline);
-		}
-	});
-	
-	// add a minute to long break session default & remaining time
-	$(".long-break-plus").click(function(){
-		if (state == "" || state == "pause" || state == "pauseBreak") {
-			longBreakTimeDef++;
-			if (state == "pauseBreak" && count % 4 == 0)
-				remainingTime += 60;
-			deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
-			display(deadline);
-		}
-	});
-	
-	// subtract a minute from long break session default & remaining time
-	$(".long-break-minus").click(function(){
-		if (remainingTime > 61 && longBreakTimeDef > 1 && (state == "" || state == "pause" || state == "pauseBreak") ) {
-			longBreakTimeDef--;
-			if (state == "pauseBreak" && count % 4 == 0)
-				remainingTime -= 60;
-			deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
-			display(deadline);
-		}
-	});
-	
-	$(".reset").click(function() {
-        clearInterval(timeInterval);
-		remainingTime = workTimeDef * 60;
-		state = "";
-        bgColor = bgGreen;
-		display(deadline);
-	});
-	
-	
-	
+$(document).ready(function () {
+  // when timer is clicked, change state
+  $(".filler").click(function () {
+    toggle();
+  });
+
+  // add a minute to work session default & remaining time
+  $(".work-plus").click(function () {
+    if (state == "" || state == "pause" || state == "pauseBreak") {
+      workTimeDef++;
+      if (state == "pause") remainingTime += 60;
+      deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
+      display(deadline);
+    }
+  });
+
+  // subtract a minute from work session default & remaining time
+  $(".work-minus").click(function () {
+    if (
+      remainingTime > 61 &&
+      workTimeDef > 1 &&
+      (state == "" || state == "pause" || state == "pauseBreak")
+    ) {
+      workTimeDef--;
+      if (state == "pause") remainingTime -= 60;
+      deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
+      display(deadline);
+    }
+  });
+
+  // add a minute to break session default & remaining time
+  $(".break-plus").click(function () {
+    if (state == "" || state == "pause" || state == "pauseBreak") {
+      breakTimeDef++;
+      if (state == "pauseBreak") remainingTime += 60;
+      deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
+      display(deadline);
+    }
+  });
+
+  // subtract a minute from break session default & remaining time
+  $(".break-minus").click(function () {
+    if (
+      remainingTime > 61 &&
+      breakTimeDef > 1 &&
+      (state == "" || state == "pause" || state == "pauseBreak")
+    ) {
+      breakTimeDef--;
+      if (state == "pauseBreak") remainingTime -= 60;
+      deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
+      display(deadline);
+    }
+  });
+
+  // add a minute to long break session default & remaining time
+  $(".long-break-plus").click(function () {
+    if (state == "" || state == "pause" || state == "pauseBreak") {
+      longBreakTimeDef++;
+      if (state == "pauseBreak" && count % 4 == 0) remainingTime += 60;
+      deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
+      display(deadline);
+    }
+  });
+
+  // subtract a minute from long break session default & remaining time
+  $(".long-break-minus").click(function () {
+    if (
+      remainingTime > 61 &&
+      longBreakTimeDef > 1 &&
+      (state == "" || state == "pause" || state == "pauseBreak")
+    ) {
+      longBreakTimeDef--;
+      if (state == "pauseBreak" && count % 4 == 0) remainingTime -= 60;
+      deadline = new Date(Date.parse(new Date()) + remainingTime * 1000);
+      display(deadline);
+    }
+  });
+
+  $(".reset").click(function () {
+    clearInterval(timeInterval);
+    remainingTime = workTimeDef * 60;
+    state = "";
+    bgColor = bgGreen;
+    display(deadline);
+  });
 });
--- main.js
+++ main.js
@@ -1,60 +1,102 @@
-const electron = require('electron')
+const electron = require("electron");
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
+const Tray = electron.Tray;
+const Menu = electron.Menu;
 
-const path = require('path')
-const url = require('url')
+const path = require("path");
+const url = require("url");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
+let isQuiting;
+let tray;
 
-function createWindow () {
+app.on("before-quit", function () {
+  isQuiting = true;
+});
+
+function createWindow() {
+  tray = new Tray(path.join(__dirname, "/img/icons/tomato.png"));
+
+  tray.setContextMenu(
+    Menu.buildFromTemplate([
+      {
+        label: "Show App",
+        click: function () {
+          mainWindow.restore();
+        },
+      },
+      {
+        label: "Quit",
+        click: function () {
+          isQuiting = true;
+          app.quit();
+        },
+      },
+    ])
+  );
+  tray.on("click", function () {
+    mainWindow.show();
+  });
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 600, height: 768, icon: __dirname + "/img/icons/tomato.png"}) //frame: false
+  mainWindow = new BrowserWindow({
+    width: 600,
+    height: 768,
+    icon: __dirname + "/img/icons/tomato.png",
+  }); //frame: false
 
   // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on("closed", function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
+  mainWindow.on("minimize", function (event) {
+    if (!isQuiting) {
+      event.preventDefault();
+      mainWindow.hide();
+      event.returnValue = false;
+    }
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on("window-all-closed", function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', function () {
+app.on("activate", function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
-
+});
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
